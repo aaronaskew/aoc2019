bbc3251bc95d662f671e84754b6c6f005d43c0b7
Aaron Askew <aaronaskew@gmail.com>
2024-12-28 19:59:11 -0600

    init commit

---
+++ day-_/src/bin/part1.rs
@@ -0,0 +1,22 @@
+fn main() {
+    let input = include_str!("input1.txt");
+    let output = process(input);
+    dbg!(output);
+}
+
+fn process(input: &str) -> String {
+    todo!("{input}")
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn example() {
+        let result = process("");
+
+        assert_eq!(result, "");
+    }
+}
